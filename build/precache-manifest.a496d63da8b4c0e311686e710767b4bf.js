5a4dbb45c4023cef0411a93dff7e40e23caad3b5
iddel_ssdc <logan.drum@iddel.ca>
2022-10-29 23:00:18 -0230

    Built production website

---
+++ build/precache-manifest.a496d63da8b4c0e311686e710767b4bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cf601e5517c0edb48a81c07ab28c39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84f2a83e44fcb9bf1b33",
+    "url": "/static/css/main.8357a2fb.chunk.css"
+  },
+  {
+    "revision": "aa469e4be37e97f9717f",
+    "url": "/static/js/2.1e65490d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1e65490d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f2a83e44fcb9bf1b33",
+    "url": "/static/js/main.aaa91904.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);

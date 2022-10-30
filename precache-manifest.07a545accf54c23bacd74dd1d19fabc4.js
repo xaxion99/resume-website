71a66fa0dbd3f3ebdf091c9a944cf9d023546854
iddel_ssdc <logan.drum@iddel.ca>
2022-10-30 09:30:29 -0230

    Updates

--- precache-manifest.07a545accf54c23bacd74dd1d19fabc4.js
+++ precache-manifest.07a545accf54c23bacd74dd1d19fabc4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1eef2433f88711e96d626bfa8d0c1e34",
+    "revision": "08a632923734112d124bbcd0b1374315",
     "url": "/index.html"
   },
   {
-    "revision": "6db2f9a747fbda1858ad",
-    "url": "/static/css/main.5124935d.chunk.css"
+    "revision": "79642f03134db77efb4b",
+    "url": "/static/css/main.ba134459.chunk.css"
   },
   {
     "revision": "aa469e4be37e97f9717f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.1e65490d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6db2f9a747fbda1858ad",
-    "url": "/static/js/main.8be09906.chunk.js"
+    "revision": "79642f03134db77efb4b",
+    "url": "/static/js/main.1eca6491.chunk.js"
   },
   {
     "revision": "97c002b8f8a25334b102",

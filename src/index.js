38d0a6d27a2148a12b1939d14894b56a0f594b9f
iddel_ssdc <logan.drum@iddel.ca>
2023-03-23 12:59:55 -0230

    Revert to working site

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
-ReactDOM.render(
-    <BrowserRouter basename="/resume-website/tree/gh-pages">
-        <App />
-    </BrowserRouter>, 
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <BrowserRouter basename="/resume-website">
+//         <App />
+//     </BrowserRouter>, 
+//     document.getElementById('root')
+// );
+
+ReactDOM.render(<App />, document.getElementById('root'));
